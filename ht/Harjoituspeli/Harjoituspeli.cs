3de67702fa63b1d33c3a6cffa9c4841b5f554a9e
Kelo Anton Vili-Sakari <anton.vs.kelo@student.jyu.fi>
2021-11-02 17:41:37 +0200

    kuvat

--- ht/Harjoituspeli/Harjoituspeli.cs
+++ ht/Harjoituspeli/Harjoituspeli.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System.Collections.Generic;
 using Jypeli;
 using Jypeli.Assets;
-using Jypeli.Controls;
-using Jypeli.Effects;
-using Jypeli.Widgets;
-
+/// <summary>
+/// @author Anton Kelo
+/// @version 
+///
+/// 
+/// </summary>
 public class Harjoituspeli : PhysicsGame
 {
     Vector nopeusYlos = new Vector(0, 150);
@@ -16,100 +15,164 @@ public class Harjoituspeli : PhysicsGame
     Vector nopeusVasemmalle = new Vector(-150, 0);
 
     PhysicsObject jalkapallo;
-    PhysicsObject pelaaja1;
+    PhysicsObject pelaaja;
     PhysicsObject vihu1;
     PhysicsObject vihu2;
 
+    /// <summary>
+    /// 
+    /// </summary>
     public override void Begin()
     {
         ClearGameObjects();
         ClearControls();
         LuoKentta();
         AsetaOhjaimet();
+        AddCollisionHandler(pelaaja, vihu1, CollisionHandler.DestroyTarget);
+        AddCollisionHandler(pelaaja, vihu2, CollisionHandler.DestroyTarget);
 
     }
+    /// <summary>
+    /// 
+    /// </summary>
     void LuoKentta()
     {
-        jalkapallo = PhysicsObject.CreateStaticObject(30.0, 30.0);
-        jalkapallo.Shape = Shape.Circle;
-        jalkapallo.X = 0.0;
-        jalkapallo.Y = 0.0;
-        jalkapallo.Restitution = 1.0;
-        Add(jalkapallo);
 
+        /// <summary>
+        /// 
+        /// </summary>
         TileMap ruudut = TileMap.FromLevelAsset("kentta1.txt");
         ruudut.SetTileMethod('*', LuoSeina);
-        ruudut.Execute(15.0, 15.0);
-
-        pelaaja1 = LuoPelaaja(Level.Right - 50.0, 0.0);
-
-        vihu1 = LuoVihu(Level.Right - 100.0, 20.0);
-        vihu2 = LuoVihu(Level.Left + 100.0, 20.0);
-
+        ruudut.Execute(20.0, 20.0);
         Level.CreateBorders(0.0, false);
-        Level.Background.Color = Color.Green;
+        Level.Background.Image = LoadImage("grass.jpeg");
+        Level.Background.TileToLevel();
         Camera.ZoomToLevel();
 
-    }
+        /// <summary>
+        /// 
+        /// </summary>
+        jalkapallo = PhysicsObject.CreateStaticObject(30.0, 30.0, Shape.Circle);
+        jalkapallo.X = 0.0;
+        jalkapallo.Y = 0.0;
+        jalkapallo.Restitution = 0.0;
+        jalkapallo.Image = LoadImage("eurooppa.png");
+        Add(jalkapallo);
+        /// <summary>
+        /// 
+        /// </summary>
+        pelaaja = LuoPelaaja(Level.Right - 50.0, 0.0);
+        /// <summary>
+        /// 
+        /// </summary>
+        vihu1 = LuoVihu(Level.Right - 45.0, 150.0);
+        //vihu1.Image = LoadImage("amerikkalainen.png");
+        PathFollowerBrain polkuaivo = new PathFollowerBrain(30);
+        vihu1.Brain = polkuaivo;
+        polkuaivo.Active = true;
+        List<Vector> polku = new List<Vector>();
+        polku.Add(new Vector(65.0, 150.0));
+        polku.Add(new Vector(65.0, 10));
+        polku.Add(new Vector(0.0, 0.0));
+        polkuaivo.Path = polku;
 
+        vihu2 = LuoVihu(Level.Left + 50.0, 150.0);
+        //vihu2.Image = LoadImage("amerikkalainen.png");
+        PathFollowerBrain polkuaivo2 = new PathFollowerBrain(30);
+        vihu2.Brain = polkuaivo2;
+        polkuaivo2.Active = true;
+        List<Vector> polku2 = new List<Vector>();
+        polku2.Add(new Vector(-185.0, 50.0));
+        polku2.Add(new Vector(-10.0, 20.0));
+        polku2.Add(new Vector(0.0, 0.0));
+        polkuaivo2.Path = polku2;
+
+
+
+    }
+    /// <summary>
+    /// Luodaan pelissä liikuteltava pelaaja
+    /// </summary>
+    /// <param name="x">Leveys</param>
+    /// <param name="y">Korkeus</param>
+    /// <returns>pelaajan pääohjelmaan</returns>
     PhysicsObject LuoPelaaja(double x, double y)
     {
         PhysicsObject pelaaja = new PhysicsObject(15.0, 15.0);
         pelaaja.Shape = Shape.Rectangle;
         pelaaja.X = x;
         pelaaja.Y = y;
-        pelaaja.Restitution = 0.0;
-        this.Add(pelaaja);
+        pelaaja.Restitution = 0.3;
+        pelaaja.CanRotate = false;
+        pelaaja.Tag = "pelaaja";      
+        //pelaaja.Image = LoadImage("");
+        Add(pelaaja);
         return pelaaja;
     }
-
+    /// <summary>
+    /// Luodaan kenttään fysiikkaobjekti joka luo sokkelon
+    /// </summary>
+    /// <param name="paikka">sijainti kentällä</param>
+    /// <param name="x">leveys</param>
+    /// <param name="y">korkeus</param>
     private void LuoSeina(Vector paikka, double x, double y)
     {
         PhysicsObject seina = PhysicsObject.CreateStaticObject(x, y);
         seina.Position = paikka;
-        seina.Shape = Shape.Rectangle;
-        seina.Color = Color.Gray;
+        seina.Shape = Shape.FromString("Rectangle");
+        seina.Color = Color.Black;
         this.Add(seina);
     }
-
+    /// <summary>
+    /// Luodaan pohja vihollisille jotka metsästävät jalkapalloa.
+    /// </summary>
+    /// <param name="x">leveys</param>
+    /// <param name="y">korkeus</param>
+    /// <returns></returns>
     PhysicsObject LuoVihu(double x, double y)
     {
-        vihu1 = new PhysicsObject(15.0, 15.0);
-        vihu1.Shape = Shape.Diamond;
-        vihu1.X = x;
-        vihu1.Y = y;
-        vihu1.Restitution = 0.0;
-        AddCollisionHandler(pelaaja1, vihu1,);
-        this.Add(vihu1);
-        return vihu1;
+        PhysicsObject vihu = new PhysicsObject(15.0, 15.0);
+        vihu.Shape = Shape.Diamond;
+        vihu.X = x;
+        vihu.Y = y;
+        vihu.Restitution = 0.3;
+        vihu.Image = LoadImage("amerikkalainen.png");
+
+        this.Add(vihu);
+        return vihu;
     }
-    
+    /// <summary>
+    /// Luodaan ohjaimet yhdelle pelaajalle
+    /// </summary>
     private void AsetaOhjaimet()
     {
-        Keyboard.Listen(Key.Up, ButtonState.Down, AsetaNopeus, "Liikuta Pelaaja ylös", pelaaja1, nopeusYlos);
-        Keyboard.Listen(Key.Up, ButtonState.Released, AsetaNopeus, null, pelaaja1, Vector.Zero);
+        Keyboard.Listen(Key.Up, ButtonState.Down, AsetaNopeus, "Liikuta Pelaaja ylös", pelaaja, nopeusYlos);
+        Keyboard.Listen(Key.Up, ButtonState.Released, AsetaNopeus, null, pelaaja, Vector.Zero);
 
-        Keyboard.Listen(Key.Left, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja vasen", pelaaja1, nopeusVasemmalle);
-        Keyboard.Listen(Key.Left, ButtonState.Released, AsetaNopeus, null, pelaaja1, Vector.Zero);
+        Keyboard.Listen(Key.Left, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja vasen", pelaaja, nopeusVasemmalle);
+        Keyboard.Listen(Key.Left, ButtonState.Released, AsetaNopeus, null, pelaaja, Vector.Zero);
 
-        Keyboard.Listen(Key.Right, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja oikea", pelaaja1, nopeusOikealle);
-        Keyboard.Listen(Key.Right, ButtonState.Released, AsetaNopeus, null, pelaaja1, Vector.Zero);
+        Keyboard.Listen(Key.Right, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja oikea", pelaaja, nopeusOikealle);
+        Keyboard.Listen(Key.Right, ButtonState.Released, AsetaNopeus, null, pelaaja, Vector.Zero);
 
-        Keyboard.Listen(Key.Down, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja alas", pelaaja1, nopeusAlas);
-        Keyboard.Listen(Key.Down, ButtonState.Released, AsetaNopeus, null, pelaaja1, Vector.Zero);
+        Keyboard.Listen(Key.Down, ButtonState.Down, AsetaNopeus, "Liikuta pelaaja alas", pelaaja, nopeusAlas);
+        Keyboard.Listen(Key.Down, ButtonState.Released, AsetaNopeus, null, pelaaja, Vector.Zero);
 
 
         Keyboard.Listen(Key.Escape, ButtonState.Pressed, ConfirmExit, "Lopeta peli");
     }
-
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="pelaaja1"></param>
+    /// <param name="nopeus"></param>
     private void AsetaNopeus(PhysicsObject pelaaja1, Vector nopeus)
     {
        pelaaja1.Velocity = nopeus;
     }
 
-    /*private void PalloOsui()
-    {
-        PosautaVihollinen()
-    }*/
+
+
 }
+
 
